38044fa9a74a6edb21ca00301cf4c00d7075d520
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2023-05-04 22:02:15 +1000

    changing PORT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const generateUniqueId = require('generate-unique-id');
 const { readFromFile, readAndAppend } = require('./utils/fsUtils');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
