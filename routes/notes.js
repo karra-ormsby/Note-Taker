e244a6f5af0c27feba357e5b4ed376d6ba3952e0
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2023-05-07 18:38:52 +1000

    used modular routing to clean up files

---
+++ routes/notes.js
@@ -0,0 +1,86 @@
+const note = require('express').Router();
+const fs = require('fs');
+const generateUniqueId = require('generate-unique-id');
+
+//Get all saved notes
+note.get('/', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            res.json(JSON.parse(data))
+        }
+    })
+});
+
+//Save new note
+note.post('/', (req, res) => {
+    res.json(`${req.method} request received`);
+
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+
+    // If all the required properties are present then we can save the new note
+    if (title && text) {
+        // Object to store the new note in
+        const newNote = {
+            title,
+            text,
+            id: generateUniqueId(),
+        };
+
+        //reading the saved data from its file
+        fs.readFile('./db/db.json', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedData = JSON.parse(data);
+                //adding the new note to the end of the notes
+                parsedData.push(newNote);
+                const stringData = JSON.stringify(parsedData, null, 4);
+                //re-writting the save file with the newly created data
+                fs.writeFile('./db/db.json', stringData, (err) => {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.log("Data written to db.json");
+                    }
+                });
+            }
+        });
+    } else {
+        res.json('Error in adding new note.');
+    }
+});
+
+//Delte specific note, chosen by id
+note.delete('/:id', (req, res) => {
+    //reading the saved data from its file
+    fs.readFile('./db/db.json', (err, data) => {
+        let newNotesArray
+        if (err) {
+            console.error(err);
+        } else {
+            const noteId = req.params.id;
+            const parsedData = JSON.parse(data);
+
+            //filter through all the notes in teh array. If their ID does not match the one we are searching for, then add it to a new array
+            newNotesArray = parsedData.filter((currNote) => {
+                return currNote.id != noteId;
+            });
+
+            //re-writting the save file with the newly created array
+            fs.writeFile('./db/db.json', JSON.stringify(newNotesArray, null, 4), (err) => {
+                if (err) {
+                    console.error(err);
+                    res.status(400).json(err);
+                } else {
+                    console.log("Data written to db.json");
+                    res.status(200).json(newNotesArray);
+                }
+            });
+        }
+    });
+});
+
+module.exports = note;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-const generateUniqueId = require('generate-unique-id');
+const api = require('./routes/index.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,94 +8,19 @@ const PORT = process.env.PORT || 3001;
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 app.use(express.static('public'));
 
-// GET Route for notes page
+// Get route for notes page
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-//Get all notes
-app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', (err, data) => {
-        if (err) {
-                console.error(err);
-            } else {
-            res.json(JSON.parse(data))
-        }
-    })
-});
-
-//Save new note
-app.post('/api/notes', (req, res) => {
-    res.json(`${req.method} request received`);
-
-    // Destructuring assignment for the items in req.body
-    const { title, text } = req.body;
-
-    // If all the required properties are present
-    if (title && text) {
-        // Variable for the object we will save
-        const newNote = {
-            title,
-            text,
-            id: generateUniqueId(),
-        };
-
-        fs.readFile( './db/db.json', (err, data) => {
-            if (err) {
-                console.error(err);
-            } else {
-                const parsedData = JSON.parse(data);
-                parsedData.push(newNote);
-                const stringData = JSON.stringify(parsedData, null, 4);
-                fs.writeFile('./db/db.json', stringData, (err) => {
-                    if (err) {
-                        console.error(err);
-                    } else {
-                        console.log("Data written to db.json");
-                    }
-                });
-            }
-        });
-    } else {
-        res.json('Error in adding new note.');
-    }
-});
-
+// Catch all for if the user enters an invalid path
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/index.html'))
 );
-
-//Delte specific note, chosen by id
-app.delete('/api/notes/:id', (req, res) => {
-    fs.readFile('./db/db.json', (err, data) => {
-        let newNotesArray
-        if (err) {
-            console.error(err);
-        } else {
-            const noteId = req.params.id;
-            const parsedData = JSON.parse(data);
-
-            newNotesArray = parsedData.filter((currNote) => {
-                return currNote.id != noteId;
-            });
-
-            fs.writeFile('./db/db.json', JSON.stringify(newNotesArray, null, 4), (err) => {
-                if (err) {
-                    console.error(err);
-                    res.status(400).json(err);
-                } else {
-                    console.log("Data written to db.json");
-                    res.status(200).json(newNotesArray);
-                }
-            });
-        }
-    });
-})
-
-
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} `)
